c3b2cc51a2787a131f05083d43d23cdbcb096b9d
Samly Panton <smov.org@gmail.com>
2020-04-08 05:01:36 +0800

    Debuging - challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -7,29 +7,30 @@ const covid19ImpactEstimator = (data) => {
   };
 };
 
-export default covid19ImpactEstimator = (data) =  {
-    region: {
-        name: "Africa",
-        avgAge: 19.7,
-        avgDialyIncomeInUSD: 5,
-        avgDialyIncomePopulation: 0.71
-    }, 
-    periodType: "days",
+export default covid19ImpactEstimator (data) = {
+  region:{
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDialyIncomeInUSD: 5,
+    avgDialyIncomePopulation: 0.71
+  }, 
+    periodType: 'days',
     timeToElapse: 58,
     reportedCases: 674,
     population: 6662275,
     totalHopitalBeds: 1380614
-};
+}
 
-function currentlyInfected() {
-    data = (reportedCases * 10)
-    return result; 
+
+function currentlyInfected(persons) {
+  persons = (reportedCases * 10)
+  return value; 
 }
 currentlyInfected();
 
-function impactcurrentlyInfected() {
-    impact = (reportedCases * 50)
-    return result;
+function impactcurrentlyInfected(impact) {
+  impact = (reportedCases * 50)
+  return value;
 }
 impactcurrentlyInfected();
 
@@ -40,19 +41,19 @@ function severecurrentlyInfected() {
 severecurrentlyInfected();
 
 function infectionsByRequestedTime() {
-    persons = currentlyInfected * (2^9)
-    return value;
+    persons = currentlyInfected * (2 ^ 9)
+    return value; 
 }
 infectionsByRequestedTime();
 
 function ImpactInfectionsByRequestedTime() {
-    impact = impactcurrentlyInfected * (2^9)
+    impact = impactcurrentlyInfected * (2 ^ 9)
     return value;
 }
 ImpactInfectionsByRequestedTime();
 
 function severeinfectionsByRequestedTime() {
-    severeImpact = severecurrentlyInfected * (2^9)
+    severeImpact = severecurrentlyInfected * (2 ^ 9)
     return value;
 }
 severeinfectionsByRequestedTime();
