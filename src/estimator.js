56f66bf5c77e72bda9ce3df2d1d8a8cc370b4afa
Samly Panton <smov.org@gmail.com>
2020-04-08 23:25:17 +0800

    Debugging - Challenge ...

--- src/estimator.js
+++ src/estimator.js
@@ -8,24 +8,25 @@ const covid19ImpactEstimator = (data) => {
 };
 module.exports = covid19ImpactEstimator;
 
-const covid19Data = (data) => {
+const covid19ImpactData = (data) => {
   const region = data; 
   return {
     data: region,
     name: 'Africa',
     avgAge: 19.7,
     avgDialyIncomeInUSD: 5,
-    avgDialyIncomePopulation: 0.71,
-  // }, 
+    avgDialyIncomePopulation: 0.71, 
     periodType: 'days',
     timeToElapse: 58,
     reportedCases: 674,
     population: 6662275,
     totalHopitalBeds: 1380614
  };
-};
 
-var reportedCases = 674, 
+};
+module.exports = covid19ImpactData;
+
+let reportedCases = 674, 
   currentlyInfectedPatients = 10;
 
 function currentlyInfected() {
@@ -33,40 +34,40 @@ function currentlyInfected() {
 }
 console.log(currentlyInfected());
 
-var reportedCases = 674,
-  impactcurrentlyInfectedPatients = 50;
+//let reportedCases = reportedCases,
+  let impactcurrentlyInfectedPatients = 50;
 
 function impactcurrentlyInfected() {
   return reportedCases * impactcurrentlyInfectedPatients;   
 }
 console.log(impactcurrentlyInfected());
 
-var reportedCases = 674,
-  severecurrentlyInfectedPatients = 50;
+//let reportedCases = reportedCases,
+  let severecurrentlyInfectedPatients = 50;
 
 function severecurrentlyInfected() {
   return reportedCases * severecurrentlyInfectedPatients;   
 }
 console.log(severecurrentlyInfected());
 
-var currentlyInfectedPersons = 6740,
-  doubledays = 512;
+let currentlyInfectedPersons = 6740,
+ doubledays = 512;
 
 function infectionsByRequestedTime() {
   return currentlyInfectedPersons * doubledays; 
 }
 console.log(infectionsByRequestedTime());
 
-var InfectionsImpactNumber = 33700,
-  doubledays = 512
+let InfectionsImpactNumber = 33700;
+  //doubledays = 512
 
 function ImpactInfectionsByRequestedTime() {
   return InfectionsImpactNumber * doubledays;
 }
 console.log(ImpactInfectionsByRequestedTime());
 
-var severeInfectionsNumber = 33700,
-  doubledays = 512;
+let severeInfectionsNumber = 33700;
+  //doubledays = 512;
 
 function severeinfectionsByRequestedTime() {
   return severeInfectionsNumber * doubledays;
