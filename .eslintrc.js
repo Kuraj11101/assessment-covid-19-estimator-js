3d6b3c325863d71624d0d592600668f7322a1a70
Samly Panton <smov.org@gmail.com>
2020-04-09 09:12:27 +0800

    debuggin

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,15 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
+    ecmaFeatures: {
+        "jsx": true,
+        "module": true,
+        "experimentalObjectRestSpread": true
+    }
   },
   rules: {
     "comma-dangle": ["error", "never"],
