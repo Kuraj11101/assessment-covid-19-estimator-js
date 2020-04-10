d4d8122c68f29bec3e7ed66c437d6afe8aaea6b4
Samly Panton <smov.org@gmail.com>
2020-04-10 18:40:14 +0800

    update

--- src/estimator.js
+++ src/estimator.js
@@ -6,24 +6,9 @@ const covid19ImpactEstimator = (data) => {
     severeImpact: {}
   };
 };
-console.log(covid19ImpactEstimator());
-
-function covid19Data() {
-  return {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDialyIncomeInUSD: 5,
-      avgDialyIncomePopulation: 0.71
-    },
-    periodType: 'days',
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 6662275,
-    totalHopitalBeds: 1380614
-  };
-}
-console.log(covid19Data());
+module.exports = {
+  covid19ImpactEstimator
+};
 
 function covid19Estimator() {
   const reportedCases = 674;
@@ -80,53 +65,3 @@ function covid19Estimator() {
   };
 }
 console.log(covid19Estimator());
-
-function impactcurrentlyInfected() {
-  const impactcurrentlyInfectedPatients = 50;
-  const reportedCases = 674;
-
-  return reportedCases * impactcurrentlyInfectedPatients;
-}
-console.log(impactcurrentlyInfected());
-
-function severecurrentlyInfected() {
-  const severecurrentlyInfectedPatients = 50;
-  const reportedCases = 674;
-
-  return reportedCases * severecurrentlyInfectedPatients;
-}
-console.log(severecurrentlyInfected());
-
-function ImpactInfectionsByRequestedTime() {
-  const InfectionsImpactNumber = 33700;
-  const doubledays = 512;
-
-  return InfectionsImpactNumber * doubledays;
-}
-console.log(ImpactInfectionsByRequestedTime());
-
-function severeinfectionsByRequestedTime() {
-  const severeInfectionsNumber = 33700;
-  const doubledays = 512;
-
-  return severeInfectionsNumber * doubledays;
-}
-console.log(severeinfectionsByRequestedTime());
-
-function severeCasesByRequestedTime() {
-  const percentofInfectionsByRequestedTime = 0.15;
-  const infectionsByRequestedTimeTotal = 3450880;
-  const dividedby = 100;
-
-  return (percentofInfectionsByRequestedTime / dividedby) * (infectionsByRequestedTimeTotal);
-}
-console.log(severeCasesByRequestedTime());
-
-function hospitalBedsByRequestedTime() {
-  const TotalsevereCasesByRequestedTime = 517632;
-  const totalHopitalBeds = 1380614;
-  const availableHospitalBedPercent = 0.35;
-
-  return (availableHospitalBedPercent / 100) * (totalHopitalBeds) - TotalsevereCasesByRequestedTime;
-}
-console.log(hospitalBedsByRequestedTime());
